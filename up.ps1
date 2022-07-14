17ec565127aee5b700fc87e615b813e96f300984
Oleg Jytnik <6032229+OlegJytnik@users.noreply.github.com>
2022-07-14 13:33:05 +0300

    Start app in connected mode.

--- up.ps1
+++ up.ps1
@@ -110,7 +110,6 @@ if ($ClientCredentialsLogin -ne "true") {
     Write-Host "Opening site..." -ForegroundColor Green
     
     Start-Process https://xmcloudcm.localhost/sitecore/
-    Start-Process https://www.sxastarter.localhost/
 }
 
 Write-Host ""
