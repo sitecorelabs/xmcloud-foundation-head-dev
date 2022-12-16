6e449a33b94b98642fba77346d8c06a70491d529
Ruslan Matkovskyi <rusm@sitecore.net>
2022-12-16 14:53:43 +0100

    #559148: fixed container component

--- src/sxastarter/src/components/Container.tsx
+++ src/sxastarter/src/components/Container.tsx
@@ -5,6 +5,7 @@ import {
   Placeholder,
   useSitecoreContext,
 } from '@sitecore-jss/sitecore-jss-nextjs';
+import config from 'temp/config';
 
 const BACKGROUND_REG_EXP = new RegExp(
   /[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}/gi
@@ -15,7 +16,7 @@ interface ComponentProps {
   params: ComponentParams;
 }
 
-export const Default = (props: ComponentProps): JSX.Element => {
+const DefaultContainer = (props: ComponentProps): JSX.Element => {
   const { sitecoreContext } = useSitecoreContext();
   const containerStyles = props.params && props.params.Styles ? props.params.Styles : '';
   const styles = `${props.params.GridParameters} ${containerStyles}`.trimEnd();
@@ -33,14 +34,26 @@ export const Default = (props: ComponentProps): JSX.Element => {
   }
 
   return (
-    <div className="container-wrapper" id={id ? id : undefined}>
-      <div className={`component container ${styles}`}>
-        <div className="component-content" style={backgroundStyle}>
-          <div className="row">
-            <Placeholder name={phKey} rendering={props.rendering} />
-          </div>
+    <div className={`component container-default ${styles}`} id={id ? id : undefined}>
+      <div className="component-content" style={backgroundStyle}>
+        <div className="row">
+          <Placeholder name={phKey} rendering={props.rendering} />
         </div>
       </div>
     </div>
   );
+}
+
+export const Default = (props: ComponentProps): JSX.Element => {
+  const splitStyles = props.params?.Styles?.split(" ");;  
+
+  if (splitStyles && splitStyles.includes('container')) {
+    return (
+      <div className="container-wrapper">
+        <DefaultContainer {...props} />
+      </div>
+    );
+  }
+
+  return <DefaultContainer {...props} />
 };
