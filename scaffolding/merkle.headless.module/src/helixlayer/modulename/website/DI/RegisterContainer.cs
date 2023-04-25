a3fcb22879b0c61f2a87509b6d16318447276dfb
Chet Potvin <CPotvin@degdigital.com>
2023-04-25 16:34:58 -0500

    1st iteration of generic helix module scaffolding

---
+++ scaffolding/merkle.headless.module/src/helixlayer/modulename/website/DI/RegisterContainer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace merklestarter.helixlayer.modulename.DI
+{
+    public class RegisterContainer : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+
+        }
+    }
+}
