e6e44f11c946bd6c3514bcf0590a89635c415c2d
Ruslan Matkovskyi <rusm@sitecore.net>
2023-06-08 17:36:58 +0200

    added tabindexes

--- src/sxastarter/src/components/PageContent.tsx
+++ src/sxastarter/src/components/PageContent.tsx
@@ -23,7 +23,7 @@ type ComponentContentProps = {
 const ComponentContent = (props: ComponentContentProps) => {
   const id = props.id;
   return (
-    <div className={`component content ${props.styles}`} id={id ? id : undefined}>
+    <div className={`component content ${props.styles}`} id={id ? id : undefined} tabIndex={1}>
       <div className="component-content">
         <div className="field-content">{props.children}</div>
       </div>
@@ -37,7 +37,11 @@ export const Default = (props: PageContentProps): JSX.Element => {
 
   if (!(props.fields && props.fields.Content) && !sitecoreContext?.route?.fields?.Content) {
     return (
-      <div className={`component content ${props.params.styles}`} id={id ? id : undefined}>
+      <div
+        className={`component content ${props.params.styles}`}
+        id={id ? id : undefined}
+        tabIndex={1}
+      >
         <div className="component-content">
           <div className="field-content">[Content]</div>
         </div>
