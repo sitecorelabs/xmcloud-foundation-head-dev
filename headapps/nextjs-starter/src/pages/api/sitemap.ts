526c71283fdadf97dc1797db44f398c68376f4f1
yavorsk <yavor.krastev@sitecore.com>
2025-01-23 10:42:34 +0200

    upgrade nextjs app to v22.4.0

--- headapps/nextjs-starter/src/pages/api/sitemap.ts
+++ headapps/nextjs-starter/src/pages/api/sitemap.ts
@@ -1,9 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import {
-  AxiosDataFetcher,
-  GraphQLSitemapXmlService,
-  AxiosResponse,
-} from '@sitecore-jss/sitecore-jss-nextjs';
+import { NativeDataFetcher, GraphQLSitemapXmlService } from '@sitecore-jss/sitecore-jss-nextjs';
 import { siteResolver } from 'lib/site-resolver';
 import config from 'temp/config';
 import clientFactory from 'lib/graphql-client-factory';
@@ -37,15 +33,23 @@ const sitemapApi = async (
     const sitemapUrl = isAbsoluteUrl ? sitemapPath : `${config.sitecoreApiHost}${sitemapPath}`;
     res.setHeader('Content-Type', 'text/xml;charset=utf-8');
 
-    // need to prepare stream from sitemap url
-    return new AxiosDataFetcher()
-      .get(sitemapUrl, {
-        responseType: 'stream',
-      })
-      .then((response: AxiosResponse) => {
-        response.data.pipe(res);
-      })
-      .catch(() => res.redirect('/404'));
+    try {
+      const fetcher = new NativeDataFetcher();
+      const response = await fetcher.fetch(sitemapUrl);
+
+      const reader = (response?.data as ReadableStream<Uint8Array>).getReader();
+      if (reader) {
+        while (true) {
+          const { done, value } = await reader.read();
+          if (done) break;
+          if (value) res.write(value);
+        }
+      }
+      res.end();
+    } catch (error) {
+      return res.redirect('/404');
+    }
+    return;
   }
 
   // this approache if user go to /sitemap.xml - under it generate xml page with list of sitemaps
@@ -58,7 +62,7 @@ const sitemapApi = async (
   const reqtHost = req.headers.host;
   const reqProtocol = req.headers['x-forwarded-proto'] || 'https';
   const SitemapLinks = sitemaps
-    .map((item) => {
+    .map((item: string) => {
       const parseUrl = item.split('/');
       const lastSegment = parseUrl[parseUrl.length - 1];
 
